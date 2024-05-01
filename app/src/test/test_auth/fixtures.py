d4dc42fbfb2c5c59a453ac6fe1f29bb21d78b876
Djapaa <estebmmr@mail.ru>
2024-05-01 14:12:57 +0300

    Added fixture for create user admin and his token

--- app/src/test/test_auth/fixtures.py
+++ app/src/test/test_auth/fixtures.py
@@ -8,7 +8,6 @@ from ...api_v1.auth.services import get_password_hash
 
 @pytest.fixture(scope='session')
 async def create_verify_user():
-    from .test_auth import mock_redis_set_email_verification_key
     dt = datetime(2024, 1, 1, hour=0,
                   minute=0, second=0, microsecond=0)
     async with test_async_session() as session:
@@ -27,7 +26,7 @@ async def create_verify_user():
 
 
 @pytest.fixture(scope='session')
-async def create_login_token_for_verify_user(create_verify_user):
+async def create_user_and_login_token(create_verify_user):
     dt = datetime(2025, 1, 1, hour=0,
                   minute=0, second=0, microsecond=0)
 
@@ -59,3 +58,38 @@ async def create_unverified_user():
             await mock_redis_set_email_verification_key('test3@mail.ru', 'test_uuid')
         except:
             await session.rollback()
+
+
+@pytest.fixture(scope='session')
+async def create_admin_user():
+    dt = datetime(2024, 2, 2, hour=0,
+                  minute=0, second=0, microsecond=0)
+    async with test_async_session() as session:
+        new_user = User(
+            id=100,
+            username='admin',
+            email='admin@mail.ru',
+            hashed_password=get_password_hash('12345'),
+            is_verified=True,
+            created_at=dt,
+            is_stuff=True
+        )
+        session.add(new_user)
+        try:
+            await session.commit()
+        except:
+            await session.rollback()
+
+@pytest.fixture(scope='session')
+async def create_admin_user_and_login_token(create_admin_user):
+    dt = datetime(2025, 2, 2, hour=0,
+                  minute=0, second=0, microsecond=0)
+
+    async with test_async_session() as session:
+        new_user_login_token = Token(
+            access_token='admin_token',
+            user_id=100,
+            expire_date=dt
+        )
+        session.add(new_user_login_token)
+        await session.commit()
